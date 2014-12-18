c37b4cab162ae0957a778f48d2b55032c8893332
dfdeshom <didier@cogtree.com>
2014-12-18 16:46:09 -0600

    python: only bootrap pip if necessary, add ipython

--- bootstrap-python-env.sh
+++ bootstrap-python-env.sh
@@ -3,11 +3,13 @@
 # Bootstrap python and pip
 ############################
 
-# Get pip
-wget https://bootstrap.pypa.io/get-pip.py
+if [ ! -f ~/.local/bin/pip ]; then
+    # Get pip
+    wget https://bootstrap.pypa.io/get-pip.py
 
-# install pip in user dir
-python get-pip.py --user
+    # install pip in user dir
+    python get-pip.py --user
+fi
 
-# install virtualenv, pylint, autopep8 as user
-pip install --user virtualenvwrapper pylint autopep8
+# install base python packages we need
+pip install --user virtualenvwrapper pylint autopep8 ipython
