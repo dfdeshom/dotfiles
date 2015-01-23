7902ad1ccc2ab977cd60bd4c46f384477e4c9c67
dfdeshom <didier@cogtree.com>
2015-01-23 14:02:21 -0600

    emacs: make multi term allow C-k

--- .emacs.d/lisp/init-multi-term.el
+++ .emacs.d/lisp/init-multi-term.el
@@ -1,7 +1,6 @@
 (require 'multi-term)
 (setq multi-term-program "/bin/zsh")
 
-
 (add-hook 'term-mode-hook
           (lambda ()
             (add-to-list 'term-bind-key-alist '("M-[" . multi-term-prev))
@@ -10,5 +9,15 @@
 (add-hook 'term-mode-hook
           (lambda ()
             (setq term-buffer-maximum-size 10000)))
+
+(defun term-send-yank ()
+  "Yank in term mode."
+  (interactive)
+  (yank)
+  (term-send-raw-string (current-kill 0)))
+
+(add-hook 'term-mode-hook
+          (lambda ()
+            (define-key term-raw-map (kbd "C-k") 'term-send-yank)))
 
 (provide 'init-multi-term)
