617bd6d62fd6c3cd550bbd4735be150dda74ccb5
dfdeshom <didier@cogtree.com>
2015-01-24 23:43:55 -0600

    emacs: add  option to create a new terminal when selecting from dfdeshom/helm-terms minibuffer

--- .emacs.d/lisp/custom-functions.el
+++ .emacs.d/lisp/custom-functions.el
@@ -1,28 +1,48 @@
 (require 'cl)
 (require 'helm-buffers)
  
-(defun dfdeshom/terminal-buffers ()
+;; internal functions
+(defun dfdeshom/terminal-buffers2 ()
   "Filter for buffers that are terminals only"
   (cl-loop for buf in (buffer-list)
            if (eq 'term-mode (buffer-local-value 'major-mode buf))
            collect (buffer-name buf)) )
 
-(setq dfdeshom/helm-source-terminals
-  (helm-build-sync-source "terminal buffers"
-    :candidates (lambda () (dfdeshom/terminal-buffers))
-    :fuzzy-match helm-buffers-fuzzy-matching
-    :keymap helm-buffers-ido-virtual-map
-    :action (helm-make-actions
-            "Switch to terminal buffer"
-            (lambda (candidate)
-              (helm-switch-to-buffer candidate)))))
+(defun dfdeshom/launch-term (name)
+  "Create new terminal in a buffer called NAME"
+  (multi-term)
+  (rename-buffer name)) 
 
+;; sources
+(setq dfdeshom/term-source-terminals
+      (helm-build-sync-source "terminal buffers"
+        :candidates (lambda () (dfdeshom/terminal-buffers2))
+        :fuzzy-match helm-buffers-fuzzy-matching
+        :keymap helm-buffers-ido-virtual-map
+        :action (helm-make-actions
+                 "Switch to terminal buffer"
+                 (lambda (candidate)
+                   (helm-switch-to-buffer candidate)))))
+
+(setq dfdeshom/term-source-terminal-not-found
+  (helm-build-dummy-source
+   "Launch new terminal"
+   :action (helm-make-actions
+            "Launch new terminal"
+            (lambda (candidate) (dfdeshom/launch-term candidate)))))
+
+
+(setq dfdeshom/term-source-term-list 
+      '( dfdeshom/term-source-terminals
+         dfdeshom/term-source-terminal-not-found))
+ 
+;; main helm function
 (defun dfdeshom/helm-terms ()
   "Custom helm buffer for terminals only"
   (interactive)
-  (helm-other-buffer '(dfdeshom/helm-source-terminals)
-                     "*helm terminal buffers*"))
-
+   (helm-other-buffer  
+    dfdeshom/term-source-term-list "*helm terminal buffers*"))
+  
 (global-set-key (kbd "C-x t")  'dfdeshom/helm-terms)
 
 (provide 'custom-functions)
