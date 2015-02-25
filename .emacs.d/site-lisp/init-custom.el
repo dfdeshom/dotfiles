c1231ee7d33a7c53c37902ee735fc2347d4f8dcb
dfdeshom <didier@cogtree.com>
2015-02-25 17:20:44 -0600

    don't redefine previous var

--- .emacs.d/site-lisp/init-custom.el
+++ .emacs.d/site-lisp/init-custom.el
@@ -1,8 +1,9 @@
-(defvar init-files
+(defvar custom-files
   '(dfd-ssh))
 
 (defun load-custom-files ()
-  (dolist (file init-files)
+  (dolist (file custom-files)
+    (print file)
     (require file)))
 
 (provide 'init-custom)
