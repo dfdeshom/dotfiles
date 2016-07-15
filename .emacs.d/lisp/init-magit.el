d5be2c51be634d2b28c711b92f2d499893e804a5
dfdeshom <dfdeshom@gmail.com>
2016-07-15 17:13:05 -0500

    get rid of line len warning

--- .emacs.d/lisp/init-magit.el
+++ .emacs.d/lisp/init-magit.el
@@ -2,6 +2,7 @@
 (setq magit-last-seen-setup-instructions "1.4.0")
 (global-set-key "\C-xg" 'magit-status)
 
+(setq git-commit-summary-max-length 1000)
 ;; don't let session save cursor position in git commit
 ;; buffers
 (eval-after-load 'session
