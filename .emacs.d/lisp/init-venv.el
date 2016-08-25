80fc09e1225adf1773217c249a5b51c9fa4b9138
dfdeshom <dfdeshom@gmail.com>
2016-08-25 15:48:03 -0500

    change venv location

--- .emacs.d/lisp/init-venv.el
+++ .emacs.d/lisp/init-venv.el
@@ -2,7 +2,7 @@
 (require 'virtualenvwrapper)
 (venv-initialize-interactive-shells) ;; if you want interactive shell support
 (venv-initialize-eshell) ;; if you want eshell support
-(setq venv-location "/home/dfdeshom/code/parsely/venvs")
+(setq venv-location "/home/dfdeshom/code/venvs")
 (setq-default mode-line-format (cons '(:exec venv-current-name) mode-line-format))
 
 (provide 'init-venv)
