09ede7c825a88b01f53cc5ba91fc89e677141f20
dfdeshom <dfdeshom@gmail.com>
2019-08-20 14:57:29 -0500

    typo

--- .emacs.d/lisp/init-sqlformat.el
+++ .emacs.d/lisp/init-sqlformat.el
@@ -1,4 +1,4 @@
-(require sqlformat)                                        
+(require 'sqlformat)                                        
 (setq sqlformat-command 'pgformatter)
 (add-hook 'sql-mode-hook 'sqlformat-on-save-mode)
 (provide 'init-sqlformat)
