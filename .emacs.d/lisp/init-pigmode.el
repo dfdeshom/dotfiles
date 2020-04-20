afe540a593433c122a5a6198507e611e10201f2d
dfdeshom <dfdeshom@gmail.com>
2020-04-20 10:45:06 -0500

    delete unused modes

--- .emacs.d/lisp/init-pigmode.el
+++
@@ -1,9 +0,0 @@
-;; pig mode settings
-(require 'pig-mode)
-(setq pig-executable "/user/bin/pig")
-(setq pig-executable-options '("-x" "local"))
-(setq pig-executable-prompt-regexp "^grunt> ")
-(setq pig-indent-level 4)
-(setq pig-version "0.11.0")
-
-(provide 'init-pigmode)
